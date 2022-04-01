965a49db0e024d6fa74cb9053294184284e7834b
thauling <hauling.thomas@gmail.com>
2022-04-01 08:10:37 +0100

    feat: add user service and interface

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { User } from '../user/user';
+//const baseUrl = 'http://localhost:8000/api/users';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiURL = "http://localhost:8000/api/user/";
+
+  httpOptions = {
+     headers: new HttpHeaders({
+       'Content-Type': 'application/json'
+     })
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<User[]> {
+   return this.httpClient.get<User[]>(this.apiURL)
+   .pipe(
+     catchError(this.errorHandler)
+   )
+ }
+
+ // note to self: remove type 'any' later!
+  
+ create(User: any): Observable<User> {
+   return this.httpClient.post<User>(this.apiURL, JSON.stringify(User), this.httpOptions)
+   .pipe(
+     catchError(this.errorHandler)
+   )
+ }
+
+ find(id: any): Observable<User> {
+   return this.httpClient.get<User>(this.apiURL + id)
+   .pipe(
+     catchError(this.errorHandler)
+   )
+ }
+
+ update(id: any, User: any): Observable<User> {
+   return this.httpClient.put<User>(this.apiURL + id, JSON.stringify(User), this.httpOptions)
+   .pipe(
+     catchError(this.errorHandler)
+   )
+ }
+
+ delete(id: any){
+   return this.httpClient.delete<User>(this.apiURL + id, this.httpOptions)
+   .pipe(
+     catchError(this.errorHandler)
+   )
+ }
+
+ errorHandler(error: any) {
+   let errorMessage = '';
+   if(error.error instanceof ErrorEvent) {
+     errorMessage = error.error.message;
+   } else {
+     errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+   }
+   return throwError(errorMessage);
+ }
+
+}
---
+++ src/app/user/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    id: number;
+    fullname: string;
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
