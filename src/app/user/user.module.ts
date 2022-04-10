325e16e0725ab61c8462f2b1bead6d460b41d927
thauling <hauling.thomas@gmail.com>
2022-04-10 18:14:11 +0100

    refac: rename logout since it can probably be removed, remove logout component from user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
-// import { UserRoutingModule } from './user-routing.module';
 
 import { RegisterComponent } from './register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
